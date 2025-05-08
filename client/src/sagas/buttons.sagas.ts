f978fe4331c5cc29b38bc994893c5ac78b78451e
Dirk Schumann <dirk@freunde-aus-berlin.net>
2025-05-08 13:05:41 +0200

    Button store/saga add/update added

--- client/src/sagas/buttons.sagas.ts
+++ client/src/sagas/buttons.sagas.ts
@@ -1,14 +1,28 @@
 import { SubscribeApiClient } from "src/lib/api";
-import { fork, put } from 'redux-saga/effects'
+import { all, call, fork, put, select, takeEvery } from 'redux-saga/effects'
 import { createApi } from "./api";
 import { takeFirst } from "./helper";
+import { selectors } from "../store";
 
 import * as buttons from '../store/buttons.store'
 import * as lifecycle from '../store/lifecycle.store';
+import { SubscribeButton } from "../types/buttons.types";
 
 function* buttonsSaga(): any {
     const apiClient: SubscribeApiClient = yield createApi()
     yield fork(initialize, apiClient)
+
+    yield takeEvery([buttons.UPDATE, buttons.ADD], save, apiClient)
+}
+
+function* save(api: SubscribeApiClient) {
+    const buttons: SubscribeButton[] = yield select(selectors.buttons.buttons)
+
+    yield all(
+        buttons.map((button: SubscribeButton) =>
+            call([api, api.put], 'buttons', button)
+        )
+    )
 }
 
 function* initialize(api: SubscribeApiClient) {
--- client/src/store/buttons.store.ts
+++ client/src/store/buttons.store.ts
@@ -11,15 +11,37 @@ export const initialState: State = {
 
 export const INIT = 'podlove/subscribe/buttons/INIT'
 export const SET = 'podlove/subscribe/buttons/SET'
+export const UPDATE = 'podlove/subscribe/buttons/UPDATE'
+export const ADD = 'podlove/subscribe/buttons/ADD'
 
 export const init = createAction<void>(INIT);
 export const set = createAction<SubscribeButton[]>(SET)
+export const update = createAction<SubscribeButton>(UPDATE)
+export const add = createAction<SubscribeButton>(ADD)
 
 export const reducer = handleActions<State, any>({
     [SET]: (state, { payload }: Action<SubscribeButton[]>) => ({
         ...state,
         buttons: payload
-    })
+    }),
+    [UPDATE]: (state, { payload }: Action<SubscribeButton>) => {
+        const updateButtons = state.buttons.map(button =>
+            button.id === payload.id
+                ? { ...button, ...payload }
+                : button
+        );
+
+        return {
+            ...state,
+            buttons: updateButtons
+        };
+    },
+    [ADD]: (state, { payload }: Action<SubscribeButton>) => {
+        return {
+            ...state,
+            buttons: [...state.buttons, payload]
+        }
+    }
 }, initialState);
 
 export const selectors = {
